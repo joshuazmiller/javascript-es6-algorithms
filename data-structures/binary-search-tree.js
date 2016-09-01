2c9a91bee071dbe49c5689555baa317c8b525c6c
Josh Miller <themllr@gmail.com>
2016-09-01 00:21:15 -0700

    Create binary-search-tree.js

---
+++ data-structures/binary-search-tree.js
@@ -0,0 +1,13 @@
+/**
+ * Binary search tree.
+ *
+ * Functions for: inserting a node, removeing a node, in-order transversal of whole tree, 
+ * in-order transversal from given node, post-order transversal of whole tree, 
+ * post-order transversal from given node, pre-order transversal of whole tree, 
+ * pre-order transversal from given node, find node by its value in whole tree,
+ * find node by its value in a given sub-tree, replace given child for a given parent, 
+ * find the minimum value in whole tree, find the minimum value in given sub-tree,
+ * find the maximum value in whole tree, find the maximum value in given sub-tree,
+ * check if BST is balanced, check if given node is balanced, find the height of the tree,
+ * find the diameter of the tree.
+ */
