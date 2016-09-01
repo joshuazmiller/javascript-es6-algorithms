1bce5111a82328d769197b0d8bfd5effd6e5bca2
Josh Miller <themllr@gmail.com>
2016-09-01 00:33:05 -0700

    Create heap.js

---
+++ data-structures/heap.js
@@ -0,0 +1,6 @@
+/**
+ * Binary heap
+ *
+ * Functions include turning a tree into a heap, changing a value, adding an element, 
+ * reading the top value, extracting the top value, and checking if the heap is empty
+ */
