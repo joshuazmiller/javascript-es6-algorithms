a2afccd9db572e97d67cc6377c7324de1db295dc
Josh Miller <themllr@gmail.com>
2016-09-01 00:26:04 -0700

    Create hash-table.js

---
+++ data-structures/hash-table.js
@@ -0,0 +1,7 @@
+/**
+ * Hash Table
+ *
+ * Functions for constructing a hash table, an implementation of Java's 32bitint hash,
+ * putting data into the table based on hashed key value, getting data from the table 
+ * based on a key, removing data based on a key
+ */
