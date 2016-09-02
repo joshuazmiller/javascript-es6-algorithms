148640d36690341a04e5395529d1a96f3e14d971
Josh Miller <themllr@gmail.com>
2016-09-02 02:12:25 -0700

    Update greatest-common-divisor-euclid.js

--- greatest-common-divisor-euclid.js
+++ greatest-common-divisor-euclid.js
@@ -1,5 +1,6 @@
 // Euclid's algorithm for finding greatest common divisor also known as greatest common factor.
 // Euclid's formula works for any two positive integers.
+// r stands for remainder
 function gcd(a,b){
   let r, temp;
   do {
