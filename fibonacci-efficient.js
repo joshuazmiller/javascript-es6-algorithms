0250550fcc74c0ffbf621a6834162619000e3464
Josh Miller <themllr@gmail.com>
2016-09-02 03:21:25 -0700

    Updated comment

--- fibonacci-efficient.js
+++ fibonacci-efficient.js
@@ -1,4 +1,11 @@
-// Very efficient. Can return large numbers in O(n) time.
+/**
+ * Very efficient compared to a naive solution which would have (2^n). 
+ * It can return large numbers in O(n^2) time. The reason why O(n^2) is
+ * we consider the 'for' statement as O(n) and since the addition inside 
+ * the loop is going to be with very large numbers it is not a regular
+ * addition of O(1) but rather O(n) as well. This is at least according
+ * to a lecture at UCSD. 
+ */
 function fibonacci(n){
   const arr = [];
   arr[0] = 0;
