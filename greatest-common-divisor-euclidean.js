4f761ebe21b0a78aa1b82b60b10bc7831bb0b1af
Josh Miller <themllr@gmail.com>
2016-09-03 11:48:56 -0700

    Update comment

--- greatest-common-divisor-euclidean.js
+++ greatest-common-divisor-euclidean.js
@@ -1,9 +1,9 @@
 /**
- * Euclid's algorithm for finding greatest common divisor also known as greatest common factor.
+ * This is Euclid's algorithm for finding greatest common divisor, also known as greatest common factor.
  * Euclid's formula works for any two positive integers.
  * The variable r stands for remainder.
- * Wikipedia on worst-case: "The Euclidean algorithm always needs less than O(h) divisions, 
- * where h is the number of digits in the smaller number b." In other words, very, very fast.
+ * Wikipedia on worst-case: "The Euclidean algorithm always needs less than O(h) divisions, where h is 
+ * the number of digits in the smaller number b." In other words, very, very fast.
  */
 function gcd(a,b){
   let r, temp;
