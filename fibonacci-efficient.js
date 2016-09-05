f69048c39d49b8e4bda5b1ab9721511a88418f3d
Josh Miller <themllr@gmail.com>
2016-09-04 18:18:45 -0700

    Add additional method

--- fibonacci-efficient.js
+++ fibonacci-efficient.js
@@ -15,3 +15,19 @@ function fibonacci(n){
   }
   return arr[n];
 }
+
+/**
+ * Here's another efficient approach that has the added advantage of
+ * taking a lot less memory. However, if you wanted to get a list
+ * of fibonacci numbers 0-n then you'd need to use the first method.
+ */
+function fibonacci(n){
+  let n1 = 0, n2 = 1, aux;
+  while (n > 0) {
+    aux = n1;
+    n1 = n2;
+    n2 += aux;
+    n = n - 1;
+  }
+  return n1;
+}
