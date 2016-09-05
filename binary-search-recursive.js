e191255f5a384840c21d06e718fc4a86aea8541c
Josh Miller <themllr@gmail.com>
2016-09-04 17:27:58 -0700

    Add master theorem analysis

--- binary-search-recursive.js
+++ binary-search-recursive.js
@@ -3,9 +3,16 @@
  * approach where we calculate the middle index and if the element is not found at that 
  * index then we recursively slice the array in half until we either find the key or the 
  * array is empty. The running efficiency is O(log n) since even an extremely large n in 
- * just one iteration becomes a problem of size n/2. The primary disadvantage of the 
- * recursive version compared to the iterative version is that the recursive version takes 
- * more stack space.
+ * just one iteration becomes a problem of size n/2 and the meat of the function runs
+ * in constant time. The primary disadvantage of the recursive version compared to the 
+ * iterative version is that the recursive version takes more stack space.
+ * 
+ * Master Theorem Efficiency Analysis:
+ * In terms of the Master Theorem aT(n/2) + O(n^d) we have a = 1 because each iteration 
+ * takes the same amount of work, and d = 0 because the meat of the function runs in constant 
+ * time O(1) which is O(n^0), this leaves us with d == log2(a) since log2(1) is also 0, 
+ * so according to the Theorem when d == log2(a) we get O(n^d log n) which in our case 
+ * is O(n^0 log n) which is the same as O(log n). 
  */
 function binarySearchRecursive(arr, key){
   if(!arr.length){
