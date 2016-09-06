e0a1a3a323ff8dc8092608ce4aee7e8212a7cd40
Josh Miller <themllr@gmail.com>
2016-09-06 03:57:07 -0700

    Create quicksort-hoare.js

---
+++ quicksort-hoare.js
@@ -0,0 +1,36 @@
+/**
+ *  In a Hoare quicksort a pivot is taken from the middle of the array. Both sides move from the edges inward
+ *  swapping values until they meet in the middle. This results in an average 3x fewer swaps then the Lomuto algorithm.
+ *  Its efficiency is O(n log n) although it's possible to hit O(n^2) in cases where bad pivots (pivots that do not
+ *  result in a meaningful swap) are consistently chosen.
+ */
+function quicksort(arr, left = 0, right = arr.length - 1){
+  const pivot = partitionHoare(arr, left, right);
+
+  if(left < pivot - 1){
+    quicksort(arr, left, pivot-1);
+  }
+
+  if(right > pivot){
+    quicksort(arr, pivot, right);
+  }
+
+  return arr;
+}
+
+function partitionHoare(arr, left, right){
+  const pivot = Math.floor((left + right)/2);
+
+  while(left < right){
+
+    while(arr[++left] < arr[pivot]);
+    while(arr[--right] > arr[pivot]);
+
+    if(left <= right){
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+  return left;
+}
