f63cd204c17829d2fbafa00a01d34a71dcd6a767
Josh Miller <themllr@gmail.com>
2016-09-06 03:12:39 -0700

    Delete heap.js

--- data-structures/heap.js
+++
@@ -1,6 +0,0 @@
-/**
- * Binary heap
- *
- * Functions include turning a tree into a heap, changing a value, adding an element, 
- * reading the top value, extracting the top value, and checking if the heap is empty
- */
