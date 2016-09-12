1e595f861856e35256429922a76962febd0529c0
Josh Miller <themllr@gmail.com>
2016-09-12 09:14:27 -0700

    Update algorithm

--- binary-search-iterative.js
+++ binary-search-iterative.js
@@ -1,25 +1,26 @@
 /**
- * A binary search works on a pre-sorted array, low to high. This is an iterative 
- * approach where we calculate the middle index and if the element is not found at that 
- * index then we iteratively slice the array in half until we either find the key or the 
- * array is empty. The running efficiency is O(log n) since even an extremely large n in 
- * just one iteration becomes a problem of size n/2. The primary advantage of the iterative
- * version over the recursive version is that the iterative version takes less stack space.
+ * A binary search works on a pre-sorted array, low to high. This is an iterative approach where we calculate the middle
+ * index and if the element is not found at that index then we iteratively slice the array in half until we either find 
+ * the key or the array is empty. The running efficiency is O(log n) since even an extremely large n in just one 
+ * iteration becomes a problem of size n/2. The primary advantage of the iterative version over the recursive version is
+ * that the iterative version takes less stack space.
  */
 function binarySearchIterative(arr, key){
-  while(arr.length){
-    const mid = Math.floor(arr.length / 2);
-    if(key === arr[mid]){
-      return "found";
-    } else if(key < arr[mid]){
-      // We don't do mid-1 because when we calculated mid we took the floor.
-      arr = arr.slice(0, mid);
+  let high = arr.length - 1, low = 0,
+    mid = Math.floor((high - low) / 2);
+  while(key !== arr[mid]){
+    if(mid === 0 || mid === high){
+      return -1;
+    }
+    if(key < arr[mid]){
+      high = mid + 1;
+      mid -= Math.floor((high - low) / 2);
     }else{
-      // We do mid+1 because we already know it's not the middle.
-      arr = arr.slice(mid + 1);
+      low = mid;
+      mid += Math.ceil((high - low) / 2);
     }
   }
-  return "not found";
+  return mid;
 }
 
 binarySearchIterative([-5, 8, 12, 17, 20, 60, 75, 77, 88, 913], 913);
